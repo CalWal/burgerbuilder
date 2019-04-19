8d7ba10a0a525f70a65abdb95989be8a4a719e74
CalWal <callumtrwallace@gmail.com>
2019-04-19 17:58:48 +1000

    Layout + aux components setup

--- burgerbuilder/src/App.js
+++ burgerbuilder/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Layout from "./components/Layout/Layout.js"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World!</h1>
+        <Layout>
+          <p>Testing</p>
+        </Layout>
       </div>
     );
   }
