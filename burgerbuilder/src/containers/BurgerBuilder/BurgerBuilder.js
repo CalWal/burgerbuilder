6b69efcf94103c1aa69a76bde4837858b568c46b
CalWal <callumtrwallace@gmail.com>
2019-05-06 16:17:29 +1000

    Order button now added

--- burgerbuilder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burgerbuilder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -21,7 +21,20 @@ class BurgerBuilder extends Component{
         cheese: 0,
         meat: 0
       },
-      totalPrice: 4
+      totalPrice: 4,
+      purchasable: false
+    }
+
+    updatePurchaseState(ingredients){
+    
+      const sum = Object.keys(ingredients)
+      .map(igKey =>{
+        return ingredients[igKey];
+      })
+      .reduce((sum,el)=>{
+        return sum + el;
+      },0);
+      this.setState({purchasable: sum > 0 })
     }
 
     addIngredienthandler = (type) =>{
@@ -38,6 +51,7 @@ class BurgerBuilder extends Component{
         totalPrice: newPrice,
         ingredients: updatedIngredients
       })
+      this.updatePurchaseState(updatedIngredients);
     }
 
     removeIngredientHandler = (type) =>{
@@ -57,6 +71,7 @@ class BurgerBuilder extends Component{
         totalPrice: newPrice,
         ingredients: updatedIngredients
       })
+      this.updatePurchaseState(updatedIngredients);
     }
 
   render(){
@@ -74,6 +89,7 @@ class BurgerBuilder extends Component{
               ingredientRemoved={this.removeIngredientHandler}
               disabled = {disabledInfo}
               price={this.state.totalPrice}
+              purchasable={this.state.purchasable}
              />
       </Aux>
     )
