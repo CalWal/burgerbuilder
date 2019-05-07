d17e020f61cdcfdb8f06f93ab41e3b999335a4ca
CalWal <callumtrwallace@gmail.com>
2019-05-07 18:27:18 +1000

    Added Sidebar + Navigation Items

--- burgerbuilder/src/components/Navigation/Toolbar/Toolbar.js
+++ burgerbuilder/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import "./Toolbar.css"
 import Logo from "../../Logo/Logo"
+import NavigationItems from "../NavigationItems/NavigationItems"
 
 const toolbar = (props) =>(
   <header className="Toolbar">
     <div>MENU</div>
-    <Logo />
-    <nav>...</nav>
+
+        <Logo/>
+
+    <nav>
+      <NavigationItems />
+    </nav>
   </header>
 );
 
