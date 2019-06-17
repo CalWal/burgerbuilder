e7c75db56c6f5379cdf49a7c9f3eeef980522811
CalWal <callumtrwallace@gmail.com>
2019-06-17 11:51:19 +1000

    Updates

---
+++ precache-manifest.994d2dcbfcf223c341a2a8df6dbf7e76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/burgerbuilder/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burgerbuilder/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "01914cde668e6b8a49bd",
+    "url": "/burgerbuilder/static/js/runtime~main.d8eea470.js"
+  },
+  {
+    "revision": "aaa12c7588236dc7fff4",
+    "url": "/burgerbuilder/static/js/main.d9e3717f.chunk.js"
+  },
+  {
+    "revision": "58f9042ad89072b04785",
+    "url": "/burgerbuilder/static/js/2.90c72c68.chunk.js"
+  },
+  {
+    "revision": "aaa12c7588236dc7fff4",
+    "url": "/burgerbuilder/static/css/main.62526d52.chunk.css"
+  },
+  {
+    "revision": "fc56fb1f28679a6a14da2faea366d489",
+    "url": "/burgerbuilder/index.html"
+  }
+];
